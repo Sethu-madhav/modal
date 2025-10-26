2936e701d1f039f43e0ac0f76628da51735f7607
Sethu-madhav <30382594+Sethu-madhav@users.noreply.github.com>
2025-10-26 19:18:48 -0400

    update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print("Hello world!")
+import modal
+import subprocess
+
+image = modal.Image.debian_slim().uv_pip_install("torch")
+app = modal.App(name="test-app", image=image)
+
+
+@app.function(gpu="A10")
+def test(name: str):
+    print(f"hello {name}")
+    print("Running nvidia-smi...")
+    output = subprocess.run(["nvidia-smi"], capture_output=True, text=True, check=True)
+    print(f"{output.stdout}")
+
+
+@app.local_entrypoint()
+def main():
+    test.remote("arjun")
